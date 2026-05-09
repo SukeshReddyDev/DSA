435ec8a27ae3393fe120fcfc5d23d5624bb52fbd
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-05-09 21:38:14 +0530

    Basic in java

---
+++ basic/swapnumbers.java
@@ -0,0 +1,11 @@
+public class swapnumbers{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp;
+        temp = a;
+        a=b;
+        b=temp;
+        System.out.println("After Swapping:"+"a:"+a+"B:"+b);
+    }
+}
