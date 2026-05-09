368efadf8ca51e100fd39dc60885d893292372e7
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-05-09 21:52:32 +0530

    swap numbers

--- basic/swapnumbers.java
+++ basic/swapnumbers.java
@@ -1,3 +1,5 @@
+// using temp variable
+
 public class swapnumbers{
     public static void main(String[] args) {
         int a = 10;
@@ -6,6 +8,20 @@ public class swapnumbers{
         temp = a;
         a=b;
         b=temp;
+        System.out.println("After Swapping:"+"a:"+a+"B:"+b);
+    }
+}
+
+
+//Without third variable
+
+public class swapnumbers{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        a=a+b;
+        b=a-b;
+        a=a-b;
         System.out.println("After Swapping:"+"a:"+a+"B:"+b);
     }
 }
