82a90d34c29b2f76c3f548393cdc741e08713139
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-05-10 21:40:11 +0530

    palindrome number

---
+++ basic/palindromenum.java
@@ -0,0 +1,18 @@
+public class palindromenum{
+    public static void main(String[] args) {
+        int num = 121;
+        int a = num;
+        int rev = 0;
+        while(num>0){
+            rev = rev * 10 + num%10;
+            num = num/10;
+        }
+        System.out.println(rev);
+        if(a==rev){
+            System.out.println(a+" is palindrome");
+        }
+        else{
+            System.out.println(a+" is not palindrome");
+        }
+    }
+}
