27c9e2b0835e3b320944b822ef5dc5258a9e2bc7
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-05-13 20:38:08 +0530

    pattern 2

--- striverbasic/patterns.java
+++ striverbasic/patterns.java
@@ -1,6 +1,7 @@
 public class patterns{
     public static void main(String[] args) {
         pattern1(4);
+        pattern2(5);
         
     }
 
@@ -11,5 +12,15 @@ public class patterns{
             }
             System.out.println();
         }
+    }
+
+    static void pattern2(int n){
+         for(int row = 1;row<=n;row++){
+            for(int col = 1;col<=row;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
