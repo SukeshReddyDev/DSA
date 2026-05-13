1ac26e10d9a6edbe771d93c45cb5c183f7f12556
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-05-13 20:51:52 +0530

    pattern 4

--- striverbasic/patterns.java
+++ striverbasic/patterns.java
@@ -3,6 +3,7 @@ public class patterns{
         pattern1(4);
         pattern2(5);
         pattern3(4);
+        pattern4(5);
         
     }
 
@@ -29,6 +30,15 @@ public class patterns{
         for(int row = 1;row<=n;row++){
             for(int col=1;col<=row;col++){
                 System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n){
+        for(int row = 1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print(row+" ");
             }
             System.out.println();
         }
