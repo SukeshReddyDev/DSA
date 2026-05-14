717354fb51595150de5a831f62f4dafd2c910ae8
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-05-14 10:41:23 +0530

    pattern 8

--- striverbasic/pattern1.java
+++ striverbasic/pattern1.java
@@ -1,6 +1,7 @@
 public class pattern1{
     public static void main(String[] args) {
         pattern7(5);
+        pattern8(5);
         
     }
 
@@ -11,6 +12,20 @@ public class pattern1{
                 System.out.print(" ");
             }
             for(int col =1;col<=(2*row)-1;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern8(int n){
+        for(int row =1;row<=n;row++){
+
+            for(int s=1;s<=row-1;s++){
+                System.out.print(" ");
+            }
+
+            for(int col = 1; col<=((2*n)-(2*row)+1);col++){
                 System.out.print("*");
             }
             System.out.println();
