7ef8634d6f60f55c7a2d00fd893415b8f49e7b8b
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-05-26 20:50:20 +0530

    soted and rotated

---
+++ arrays/sortrotate.java
@@ -0,0 +1,27 @@
+
+
+class sortrotate{
+    public static void main(String[] args) {
+        int[] arr = {2,1,3,4};
+        boolean ans = rotate(arr);
+        System.out.println(ans);
+
+    }
+    
+    static boolean rotate(int[] arr){
+        int count =0;
+        int n =arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]>arr[(i+1)%n]){
+                count++;
+            }
+        }
+        if (count>1){
+            return false;
+        
+        }
+        else{
+            return true;
+        }
+    }
+}
