f2e25f2a74f8f6b524714354d778cae6adec7b3d
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-06-02 18:03:35 +0530

    optimal for move zeros to end

--- arrays/movezeros.java
+++ arrays/movezeros.java
@@ -25,3 +25,38 @@ class Main {
         System.out.println(Arrays.toString(arr));
     }
 }
+
+
+// optimized approach
+
+
+class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,0,2,0,3,0,4,5,6,7};
+        swap(nums);
+
+    }
+    static void swap(int[] nums){
+        int j = -1;
+        for(int i=0;i < nums.length;i++){
+            if(nums[i] == 0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1){
+            System.out.println("No zeros present");
+            System.out.println(Arrays.toString(nums));
+            return;
+        }
+        for(int i =j+1;i<nums.length;i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
