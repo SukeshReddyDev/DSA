38d58d87588bb2740aec605ada9ea70ae456440a
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-06-08 20:36:14 +0530

    optimal sol

--- arrays/union.java
+++ arrays/union.java
@@ -22,3 +22,78 @@ class union {
     }
 }
 
+// Optimal Solution
+
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1,2,2,3,4};
+        int[] arr2 = {2,3,5};
+
+        ArrayList<Integer> ans = union(arr1, arr2);
+
+        System.out.println(ans);
+    }
+
+    static ArrayList<Integer> union(int[] arr1, int[] arr2) {
+
+        int i = 0;
+        int j = 0;
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        while(i < arr1.length && j < arr2.length) {
+
+            if(arr1[i] < arr2[j]) {
+
+                if(!ans.contains(arr1[i])) {
+                    ans.add(arr1[i]);
+                }
+
+                i++;
+            }
+
+            else if(arr1[i] > arr2[j]) {
+
+                if(!ans.contains(arr2[j])) {
+                    ans.add(arr2[j]);
+                }
+
+                j++;
+            }
+
+            else {
+
+                if(!ans.contains(arr1[i])) {
+                    ans.add(arr1[i]);
+                }
+
+                i++;
+                j++;
+            }
+        }
+
+        while(i < arr1.length) {
+
+            if(!ans.contains(arr1[i])) {
+                ans.add(arr1[i]);
+            }
+
+            i++;
+        }
+
+        while(j < arr2.length) {
+
+            if(!ans.contains(arr2[j])) {
+                ans.add(arr2[j]);
+            }
+
+            j++;
+        }
+
+        return ans;
+    }
+}
