27f8da269945863377f390facca7ad04839f072b
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-06-12 13:46:04 +0530

    intersection optimal

---
+++ arrays/intersection.java
@@ -0,0 +1,33 @@
+// Optimal Solution
+
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int[] arr1 ={1,2,3,3,4,5,6};
+        int[] arr2 = {3,3,4,5,8,9};
+        System.out.println(intersection(arr1,arr2));
+
+    }
+    static ArrayList<Integer> intersection(int[] arr1,int[] arr2){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i=0;
+        int j=0;
+        while(i< arr1.length && j<arr2.length){
+            if(arr1[i]>arr2[j]){
+                j++;
+            }
+            else if (arr1[i]==arr2[j]){
+                if(ans.size()==0 || ans.get(ans.size()-1)!=arr1[i]){
+                    ans.add(arr1[i]);
+                }
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return ans;
+    }
+}
