a4802b9a7978ac7fd5a5914ed5d3255e10eb71a5
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-06-13 18:15:04 +0530

    missing number optimal

---
+++ arrays/missing.java
@@ -0,0 +1,24 @@
+// optimal Solution 
+
+
+class Main {
+    public static void main(String[] args) {
+        int[] nums = {2,1,3,4,5};
+        int ans = missing(nums);
+        System.out.println(ans);
+    }
+    
+    static int missing(int[] nums){
+        int n = nums.length;
+        int sum =0;
+        int exp_sum = (n*(n+1))/2;
+        for(int i=0;i<nums.length;i++){
+            sum = sum+nums[i];
+        }
+        
+        int res = exp_sum-sum;
+        return res;
+    }
+}
+
+// brute force solution
