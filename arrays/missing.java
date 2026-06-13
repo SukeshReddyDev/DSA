467386c7a3b4ac24249500eb97c867c0a77cfa72
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-06-13 18:15:50 +0530

    missing number brute

--- arrays/missing.java
+++ arrays/missing.java
@@ -22,3 +22,31 @@ class Main {
 }
 
 // brute force solution
+
+
+class Main {
+    public static void main(String[] args) {
+        int[] nums = {2,1,3,4,5};
+        int ans = missing(nums);
+        System.out.println(ans);
+    }
+    
+    static int missing(int[] nums){
+        int n = nums.length;
+        for(int i=0;i<=n;i++){
+            boolean found = false;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]==i){
+                    found = true;
+                    break;
+                }
+            }
+            if(found==false){
+            return i;
+            }
+        }
+        return -1;
+    }
+    
+}
+
