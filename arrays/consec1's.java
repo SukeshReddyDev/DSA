39a2abb55817c3655b848858bd576440b00a5395
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-06-14 20:37:41 +0530

    brute force

--- arrays/consec1's.java
+++ arrays/consec1's.java
@@ -25,3 +25,32 @@ class Main {
         return ans;
     }
 }
+
+// Brute force 
+/* class Main {
+    public static void main(String[] args) {
+        int[] nums={1,1,0,0,1,1,1,0,0};
+        int res = consec(nums);
+        System.out.println(res);
+        
+    }
+    
+    static int consec(int[] nums){
+        int maxcount = 0;
+        for(int i=0;i<nums.length;i++){
+            int count =0;
+            for(int j=i;j<nums.length;j++){
+                if(nums[j]==1){
+                count++;
+                }
+                else{
+                    break;
+                }
+                
+            }
+            maxcount = Math.max(maxcount,count);
+        }
+        return maxcount;
+    }
+}
+*/
