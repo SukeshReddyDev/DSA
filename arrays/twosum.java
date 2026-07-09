cfc051c09ba65aee329bb3fa6ad8f2dbc0125fc8
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-07-09 23:31:14 +0530

    hashing technique

--- arrays/twosum.java
+++ arrays/twosum.java
@@ -1,13 +1,16 @@
 import java.util.*;
-public class hello {
+public class twosum{
     public static void main(String[] args) {
 
         int[] nums = {1,6,2,10,3};
         int k = 7;
 
         int[] res = twosum(nums, k);
+        int[] res1 = twosum1(nums, k);
         System.out.println(Arrays.toString(res));
     }
+
+    // brute force
     static int[] twosum(int[] nums,int k){
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
@@ -15,6 +18,22 @@ public class hello {
                 if(sum == k){
                     return new int[]{i,j};
                 }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    // HAshing technique better solution
+    static int[] twosum1(int[] nums,int k){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int a = nums[i];
+            int rem = k-a;
+            if(map.containsKey(rem)){
+                return new int[]{map.get(rem),i};
+            }
+            else{
+                map.put(a,i);
             }
         }
         return new int[]{-1,-1};
