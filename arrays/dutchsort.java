93bda2db4c7a221758029761fdd3cb4ce3f9ec2b
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-07-10 23:28:12 +0530

    better approach

--- arrays/dutchsort.java
+++ arrays/dutchsort.java
@@ -27,4 +27,33 @@ public class dutchsort{
 
 
     }
+
+    // count digit better approach
+     static int[] sorting1(int[] nums){
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count0++;
+            }
+            else if(nums[i]==1){
+                count1++;
+            }
+            else{
+                count2++;
+            }
+        }
+        for(int i=0;i<count0;i++){
+            nums[i] = 0;
+        }
+        for(int i=count0;i<count0+count1;i++){
+            nums[i] = 1;
+        }
+        for(int i=count0+count1;i<nums.length;i++){
+            nums[i]=2;
+        }
+        return nums;
+
+    }
 }
