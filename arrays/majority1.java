9ad08cd2dc13c198408bae513df9ae970ebfad97
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-07-11 22:26:18 +0530

    hashing method

--- arrays/majority1.java
+++ arrays/majority1.java
@@ -5,6 +5,7 @@ public class majority1 {
         int[] nums = {7, 0, 0, 1, 7, 7, 2, 7, 7};
 
         int res = sorting(nums);
+        int res1 = sorting1(nums);
         System.out.println(res);
     }
     static int sorting(int[] nums){
@@ -22,4 +23,24 @@ public class majority1 {
         return -1;
 
     }
-}
+
+    static int sorting1(int[] nums){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n= nums.length;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else {
+                map.put(nums[i], 1);
+            }
+        }
+        for(int key : map.keySet()){
+            if(map.get(key)>n/2){
+                return key;
+            }
+        }
+        return -1;
+
+    }
+} 
