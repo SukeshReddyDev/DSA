93278597c6b374d3cda7f2af0f89196c5424602d
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-07-14 23:43:36 +0530

    optimal for rearange

--- arrays/arraysign.java
+++ arrays/arraysign.java
@@ -25,4 +25,27 @@ public class arraysign {
        }
        return ans;
     }
+
+
+        static int[] maxsum1(int[] nums) {
+        int pos = 0;
+        int neg = 1;
+        int[] ans = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                ans[pos] = nums[i];
+                pos += 2;
+            }
+
+            else{
+                ans[neg] = nums[i];
+                neg += 2;
+            }
+        }
+        return ans;
+
+    }
+
+
+
 }
