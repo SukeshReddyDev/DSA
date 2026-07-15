f17e7ed466f87e6b95a3261d640251b36a25c907
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-07-16 00:49:04 +0530

    optimal for leaders

--- arrays/leader.java
+++ arrays/leader.java
@@ -3,7 +3,11 @@ public class leader{
     public static void leader(String[] args) {
         int nums[] = {1,2,5,3,1,2};
         ArrayList<Integer> res = leaders(nums);
+        ArrayList<Integer> res1 = leaders1(nums);
+
         System.out.println(res);
+        System.out.println("res1");
+
 
 
 
@@ -23,6 +27,20 @@ public class leader{
                 ans.add(nums[i]);
             }
         }
+        return ans;
+
+    }
+
+    static ArrayList<Integer> leaders1(int[] nums){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for(int i=nums.length -1;i>=0;i--){
+            if(nums[i]>max){
+                max = nums[i];
+                ans.add(nums[i]);
+            }
+        }
+        Collections.reverse(ans);
         return ans;
 
     }
