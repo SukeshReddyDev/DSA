35bb5800a058467502a99be5f08a80af67bcbaba
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-07-30 23:32:46 +0530

    optimal approach

--- arrays/maxproduct.java
+++ arrays/maxproduct.java
@@ -1,7 +1,6 @@
-import java.util.*;
 
 
-public class hello{
+public class maxproduct{
     public static void main(String[] args){
         int[] nums={-5,0,-2};
         int ans = max(nums);
@@ -17,6 +16,25 @@ public class hello{
                 prod = prod*nums[j];
                 maxi = Math.max(prod,maxi);
             }
+        }
+        return maxi;
+    }
+
+    static int max1(int[] nums){
+        int maxi=Integer.MIN_VALUE;
+        int prefix =1;
+        int n = nums.length;
+        int suffix =1;
+        for(int i=0;i<nums.length;i++){
+            if(prefix==0){
+                prefix =1;
+            }
+            if(suffix ==0){
+                suffix=1;
+            }
+            prefix = prefix*nums[i];
+            suffix = suffix*nums[n-i-1];
+            maxi=Math.max(maxi,Math.max(prefix,suffix));
         }
         return maxi;
     }
