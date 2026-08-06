365b13506a95ee034ef504b1f5a99ccd9d3b49e1
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-08-06 18:42:49 +0530

    insert position

---
+++ binarysearch/insertposition.java
@@ -0,0 +1,31 @@
+
+
+
+public class insertposition{
+    public static void main(String[] args){
+        int[] nums={3,5,8,15,19};
+        int x = 9;
+        int sol = lower(nums,x);
+        System.out.println(sol);
+
+    }
+    static int lower(int[] nums,int x){
+        int ans = nums.length;
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] >= x){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+
+        }
+        return ans;
+    }
+
+}
