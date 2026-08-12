bfed2d429b287da459f2913ad9f2f1302c9a72c4
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-08-12 22:20:03 +0530

    optimal approach

--- binarysearch/searchrotated1.java
+++ binarysearch/searchrotated1.java
@@ -8,6 +8,7 @@ public class searchrotated1{
         System.out.println(res);
 
     }
+    // Linear Search approach
     static int search(int[] nums , int target){
         int n = nums.length;
         int ans = -1;
@@ -18,6 +19,34 @@ public class searchrotated1{
         }
         return ans;
     }
+
+    // Optimal approach
+
+    static int search1(int[] nums , int target){
+    int n = nums.length;
+    int low = 0;
+    int high = n-1;
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(nums[low]<=nums[mid]){
+            if(nums[low] <=target && target<= nums[mid]){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }else{
+            if(nums[mid]<=target && target<=nums[high]){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+    }
+    return -1;
+}
 
 
 }
