693689527a8e81abe77209f7fe73a1e6e094d875
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-09-04 23:28:57 +0530

    optimal approach

--- binarysearch/shipgoods.java
+++ binarysearch/shipgoods.java
@@ -21,6 +21,25 @@ public class shipgoods{
 
     }
 
+    static int mindays2(int[] nums,int days){
+        int low = max(nums);
+        int high = sum(nums);
+        int ans = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int reqdays = func(nums,mid);
+            if(reqdays<=days){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+
+
+    }
+
     static int func(int[] nums,int capacity){
         int days = 1;
         int load = 0;
