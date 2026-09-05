174a926948a5ca307991452ed11d4f8594902ca0
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-09-05 23:29:35 +0530

    brute force

---
+++ binarysearch/aggressivecows.java
@@ -0,0 +1,64 @@
+import java.util.*;
+public class aggressivecows{
+    public static void main(String[] args){
+       int[] nums = {0, 3, 4, 7, 10, 9};
+       int k = 4;
+       int ans = maxmin(nums,k);
+        System.out.println(ans);
+    }
+
+    static int maxmin(int[] nums,int k){
+        Arrays.sort(nums);
+        int ans =0;
+        for(int i=1;i<=(max(nums)-min(nums));i++){
+            if(func(nums,k,i)==true){
+                ans =i;
+            }else{
+                break;
+            }
+        }
+        return ans;
+
+    }
+
+    static boolean func(int[] nums,int k,int dis){
+        int cntcow = 1;
+        int last = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]-last>=dis){
+                cntcow++;
+                last = nums[i];
+            }
+        }
+        if(cntcow >= k){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    static int max(int[] nums){
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                max = nums[i];
+            }
+        }
+        return max;
+
+    }
+
+    static int min(int[] nums){
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<min){
+                min = nums[i];
+            }
+        }
+        return min;
+
+    }
+
+
+
+}
