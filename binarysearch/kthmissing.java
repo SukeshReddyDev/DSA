1bddf01efed1b162af0653d4e078c68599a48194
SukeshReddyDev <venkatasukeshreddynallareddy@gmail.com>
2026-09-05 22:18:21 +0530

    optimal approach

--- binarysearch/kthmissing.java
+++ binarysearch/kthmissing.java
@@ -19,5 +19,22 @@ public class kthmissing{
 
     }
 
+    static int missing2(int[] nums,int k){
+        int low =0;
+        int high = nums.length -1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            int missing = nums[mid]-(mid+1);
+            if(missing<k){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return high+k+1;
+
+
+    }
+
 
 }
